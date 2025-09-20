b9c035b4b3fe14a8d98383940ba8244dd4086db2
Henry Nilson <hnilson0@byui.edu>
2025-09-20 13:43:46 -0600

    i did it

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("what is your grade percentage?");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Congrats you have an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You are a failure and a dissapiontment you have a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You have a C did you even try?");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You have a D you should be ashamed of yourself");
+        }
+        else
+        {
+            Console.WriteLine("You have an F you are a disgrace to your family");
+        }
     }
 }
